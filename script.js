737c68c670ea69787615c804f5865b5e14a4944b
Ryan <yeasinryan00&@gmail.com>
2025-09-15 12:41:33 +0600

    Initial commit - basic to do list

---
+++ script.js
@@ -0,0 +1,29 @@
+const taskInput = document.querySelector('#taskInput');
+const addTaskBtn = document.querySelector('#addTaskBtn');
+const todoList = document.querySelector('#todoList');
+
+function createTask(userText){
+    const li = document.createElement('li');
+    li.textContent = userText;
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    li.appendChild(deleteBtn);
+    todoList.appendChild(li);
+}
+
+addTaskBtn.addEventListener('click', () => {
+    const userText = taskInput.value.trim();
+    createTask(userText);
+    taskInput.value = '';
+})
+
+todoList.addEventListener('click', (e) => {
+    const clicked = e.target;
+    if(clicked.tagName === 'LI'){
+        clicked.classList.toggle('completed');
+    }
+
+    if(clicked.tagName === 'BUTTON'){
+        clicked.parentElement.remove();
+    }
+})
